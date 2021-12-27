8735f81823b07610630f9274d1b5f307993b4103
Preda Alexandru <alexpreda95@yahoo.com>
2021-12-27 13:19:02 +0200

    Second fixed on fetchApi

--- real-estate/utils/fetchApi.js
+++ real-estate/utils/fetchApi.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
-// headers: {
-//     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-//     'x-rapidapi-key': '51d3cb8d09msh4144b0dfffa7ed9p18514cjsnaa0b73a8ef56'
-//   }
 
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
