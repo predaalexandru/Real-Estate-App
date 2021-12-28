ec7b4cec8e86d2feaecf4edcd0ed2929068f7263
Preda Alexandru <alexpreda95@yahoo.com>
2021-12-28 08:01:50 +0200

    Hide API Key

--- real-estate/utils/fetchApi.js
+++ real-estate/utils/fetchApi.js
@@ -7,7 +7,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
       headers: {
         'x-rapidapi-host': 'bayut.p.rapidapi.com',
-        'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+        'x-rapidapi-key': process.env.RAPID_API_KEY,
       },
     });
       
